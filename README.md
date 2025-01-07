b28bd22c82e04719a53dedbeaa58a2a4ecba88dc
sungmin choi <namja306@naver.com>
2025-01-07 17:02:00 +0900

    question model

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# devops_quiz
+# DevOps 퀴즈 앱
 
-A simple Flutter application designed to test and improve DevOps knowledge through fun, interactive quizzes. This project focuses on various DevOps-related topics, including Docker, Kubernetes, Linux, networking, continuous integration and delivery (CI/CD), and infrastructure as code (IaC).
+DevOps 관련 지식을 간단하고 재미있게 익힐 수 있도록 제작한 Flutter 애플리케이션입니다. Docker, Kubernetes, Linux, 네트워크, CI/CD, IaC 등 다양한 DevOps 주제를 다루며, 사용자는 퀴즈를 통해 학습하고 실력을 점검할 수 있습니다.
---
+++ lib/models/question.dart
@@ -0,0 +1,125 @@
+enum QuestionType {
+  singleChoice,
+  multipleChoice,
+  trueFalse,
+  fillInTheBlank,
+}
+
+enum QuestionCategory {
+  devops,
+  kubernetes,
+  docker,
+  git,
+  ciCd,
+  linux,
+  aws,
+  azure,
+  jenkins,
+  gitlab,
+  github,
+  ansible,
+  terraform,
+}
+
+enum QuestionDifficulty {
+  easy,
+  medium,
+  hard,
+}
+
+class Question {
+  final String questionId;
+  final QuestionCategory category;
+  final QuestionType questionType;
+  final QuestionDifficulty difficulty;
+  // 텍스트 문제를 위한 필드
+  final String? questionText;
+
+  // 이미지 문제를 위한 필드
+  final String? imageUrl;
+  final String? subText;
+  final dynamic answer;
+  final String? explanationText;
+  final String? referenceLink;
+
+  // 객관식 문제의 보기 목록
+  final List<String>? options;
+
+  Question({
+    required this.questionId,
+    required this.category,
+    required this.questionType,
+    required this.difficulty,
+    this.questionText,
+    this.imageUrl,
+    this.subText,
+    this.answer,
+    this.explanationText,
+    this.referenceLink,
+    this.options,
+  });
+
+  factory Question.fromMap(Map<String, dynamic> map) {
+    return Question(
+      questionId: map['questionId'] ?? '',
+      category: map['category'] ?? '',
+      questionType: _stringToQuestionType(map['questionType']),
+      difficulty: _stringToDifficulty(map['difficulty']),
+
+      // 텍스트 / 이미지 모두 받아와서 저장
+      questionText: map['questionText'],
+      imageUrl: map['imageUrl'],
+
+      subText: map['subText'],
+      answer: map['answer'],
+      explanationText: map['explanationText'],
+      referenceLink: map['referenceLink'],
+      options:
+          map['options'] != null ? List<String>.from(map['options']) : null,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'questionId': questionId,
+      'category': category,
+      'questionType': questionType.toString().split('.').last,
+      'difficulty': difficulty.toString().split('.').last,
+      'questionText': questionText,
+      'imageUrl': imageUrl,
+      'subText': subText,
+      'answer': answer,
+      'explanationText': explanationText,
+      'referenceLink': referenceLink,
+      'options': options,
+    };
+  }
+
+  static QuestionType _stringToQuestionType(String? type) {
+    switch (type) {
+      case 'singleChoice':
+        return QuestionType.singleChoice;
+      case 'multipleChoice':
+        return QuestionType.multipleChoice;
+      case 'trueFalse':
+        return QuestionType.trueFalse;
+      case 'fillInTheBlank':
+        return QuestionType.fillInTheBlank;
+      default:
+        return QuestionType.singleChoice; // 기본값
+    }
+  }
+
+  static QuestionDifficulty _stringToDifficulty(String? diff) {
+    switch (diff) {
+      case 'easy':
+        return QuestionDifficulty.easy;
+      case 'medium':
+        return QuestionDifficulty.medium;
+      case 'hard':
+        return QuestionDifficulty.hard;
+      default:
+        return QuestionDifficulty.easy; // 기본값
+    }
+  }
+}
